29893a7a6320c349e088a3210fc99337017b356e
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-10-09 22:25:06 -0400

    Practiced using jest

---
+++ src/ptui.ts
@@ -0,0 +1,5 @@
+export class Ptui {
+    add(a: number, b: number) {
+        return a + b;
+    }
+}
---
+++ tests/ptui.test.ts
@@ -0,0 +1,8 @@
+import { Ptui } from '../src/ptui';
+
+describe('Ptui', () => {
+    it("should do...", () => {
+        let p = new Ptui();
+        expect(p.add(1, 6)).toEqual(7);
+    })
+});
