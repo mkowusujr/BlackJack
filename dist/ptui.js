41dc6dfa20dbacd4e4f62e3e20803c52a2abc433
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-10-09 22:24:35 -0400

    Created dist folder

---
+++ dist/ptui.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Ptui = void 0;
+class Ptui {
+    add(a, b) {
+        return a + b;
+    }
+}
+exports.Ptui = Ptui;
+//# sourceMappingURL=ptui.js.map
