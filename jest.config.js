adf23715cdd4d20a8c939dd874db5fc0867eb45b
Mathew Owusu Jr <93245467+mkowusujr@users.noreply.github.com>
2022-10-10 20:09:18 -0400

    Updated testing config

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,13 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: -10
+    }
+  }
 };
